b44c2d2d7443abb90fe78f4177c6c8ad3bef43cd
vicnx <andanivicente@gmail.com>
2021-01-26 19:27:23 +0100

    Vue start, routing

---
+++ frontend/vuee/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div id="nav">
+      <router-link :to="{ name: 'Home' }">Home</router-link> |
+      <router-link :to="{ name: 'About' }">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+
+</template>
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+
+<style>
+.nav {
+  height: 50px;
+  width: 100%;
+  background-color: #4d4d4d;
+  position: relative;
+}
+</style>
