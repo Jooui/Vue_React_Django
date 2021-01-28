9083a22ae752d147049164212187fe5a5102211e
vicnx <andanivicente@gmail.com>
2021-01-28 10:06:01 +0100

    Añadido bot para teleram (cuando realizas un commit envia un mensaje al grupo)
    Co-authored-by: Joel Revert <jrevertvila@gmail.com>

---
+++ .github/actions/send_telegram_msg/index.js
@@ -0,0 +1,18 @@
+const core = require("@actions/core");
+const github = require("@actions/github") 
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = core.getInput("token_bot");
+const bot = new TelegramBot(token, {polling: false});
+var chatId=core.getInput("id_chat");
+const name = core.getInput("name");
+const commit = github.context.payload;
+
+try {
+  bot.sendMessage(chatId,`Hola ${name}, se ha realizado un commit con exito:\n\nAutor: ${commit.head_commit.author.username}\nEmail: ${commit.head_commit.author.email}\nMsg:${commit.head_commit.message}\nUrl:${commit.head_commit.url}`);
+
+} catch (error) {
+  core.setFailed(error.message);
+}
+
+core.setOutput("message", "Mensaje enviado con exito");
