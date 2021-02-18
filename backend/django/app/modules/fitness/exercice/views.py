fc870aaec54b755a0cec0e264d7a368b59a5ea3e
vicnx <andanivicente@gmail.com>
2021-02-18 19:56:18 +0100

    Custom permission ISOWNER for exercices
    Co-authored-by: Joel Revert <Jooui@jrevertvila@gmaoil.com>

--- backend/django/app/modules/fitness/exercice/views.py
+++ backend/django/app/modules/fitness/exercice/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets,generics,mixins,status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import (AllowAny, IsAuthenticated, IsAuthenticatedOrReadOnly)
+from app.modules.core.permissions import IsOwner
 from .serializers import ExerciceSerializer
 from .models import Exercice
 from .renderers import ExerciceJSONRenderer
@@ -13,9 +14,19 @@ class ExerciceViewSet(viewsets.ModelViewSet):
     serializer_class = ExerciceSerializer
     # Para buscar por slug
     lookup_field = 'slug'
+    # Para establecer PERMISOS a request metodos
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            self.permission_classes = [IsAuthenticated,]
+        elif self.request.method == 'PUT':
+            self.permission_classes = [IsAuthenticated,IsOwner]
+        elif self.request.method == 'DELETE':
+            self.permission_classes = [IsAuthenticated,IsOwner]
+            
+        return super(ExerciceViewSet,self).get_permissions()
+
     def get_queryset(self):
         queryset = self.queryset
-        
         author = self.request.query_params.get('author', None)
         if author is not None:
             queryset = queryset.filter(author__user__username=author)
@@ -27,8 +38,10 @@ class ExerciceViewSet(viewsets.ModelViewSet):
         return queryset
 
     def create(self, request):
+        print("CREATE VIEWS")
+        print(self.request.user)
         serializer_context = {
-            'author': request.user.profile,
+            'author': self.request.user.profile,
             'request': request,
         }
         serializer_data = request.data.get('exercice', {})
@@ -40,6 +53,12 @@ class ExerciceViewSet(viewsets.ModelViewSet):
         serializer.save()
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response("Deleted",status=status.HTTP_200_OK)
+
 
 class ExerciceFavoriteAPIView(APIView):
     permission_classes = (IsAuthenticated,)
